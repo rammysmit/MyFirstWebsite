b3ce60b8ac0a551ce1350feb6722ded45c5d8eec
Patel <rnpatel@westernasset.local>
2017-03-14 09:41:32 -0700

    my first website files

---
+++ src/main/java/com/example/MyFirstWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstWebsiteApplication.class, args);
+	}
+}
